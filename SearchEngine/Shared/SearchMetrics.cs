911e1a46b658272c100508fb017930878ac7c6e6
NicolineB <broloes2@gmail.com>
2025-05-22 11:07:52 +0200

    Sat up montoring / metrics - Unsure if actually works have to test in kubernetes.

--- SearchEngine/Shared/SearchMetrics.cs
+++ SearchEngine/Shared/SearchMetrics.cs
@@ -1,22 +1,22 @@
-﻿using OpenTelemetry.Metrics;
-using System.Diagnostics.Metrics;
+﻿using System.Diagnostics.Metrics;
 
 namespace Shared
 {
     public class SearchMetrics
     {
-        private static readonly Meter Meter = new("SearchEngineMetrics", "1.0.0");
+        public Counter<long> CacheHits { get; }
+        public Counter<long> CacheMisses { get; }
+        public Histogram<double> QueryDuration { get; }
+        public Counter<long> BackendUsed { get; }
 
-        public Counter<long> CacheHits { get; } =
-            Meter.CreateCounter<long>("search_cache_hits", "hits", "Number of cache hits");
+        public SearchMetrics(IMeterFactory meterFactory)
+        {
+            var meter = meterFactory.Create("SearchEngineAPI");
 
-        public Counter<long> CacheMisses { get; } =
-            Meter.CreateCounter<long>("search_cache_misses", "misses", "Number of cache misses");
-
-        public Histogram<double> QueryDuration { get; } =
-            Meter.CreateHistogram<double>("search_query_duration_ms", "ms", "Search query duration in ms");
-
-        public Counter<long> BackendUsed { get; } =
-            Meter.CreateCounter<long>("search_backend_used", "calls", "Backend call count");
+            CacheHits = meter.CreateCounter<long>("search_cache_hits", "hits", "Number of cache hits");
+            CacheMisses = meter.CreateCounter<long>("search_cache_misses", "misses", "Number of cache misses");
+            QueryDuration = meter.CreateHistogram<double>("search_query_duration_ms", "ms", "Search query duration in ms");
+            BackendUsed = meter.CreateCounter<long>("search_backend_used", "calls", "Backend call count");
+        }
     }
 }
